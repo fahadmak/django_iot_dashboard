790953b7f5b0bb60b2732b3da6e91a35ab1e8975
fahadmak <fahad.makabugo@andela.com>
2019-07-30 18:42:48 +0300

    Chore(Caching): install redis data caching
    - Add redis in cache settings.
    - Add the @cache_page decorator to the dashboard view function
    
    [Finishes #167556579]

--- iot_dashboard/apps/dashboard/views.py
+++ iot_dashboard/apps/dashboard/views.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.decorators import login_required
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 
+from iot_dashboard import settings
 from iot_dashboard.apps.dashboard.services import get_channel
+
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
 @login_required
+@cache_page(CACHE_TTL)
 def dashboard(request):
-    channel = get_channel()
-    return render(request, 'dashboard/index.html', {'feeds': channel['feeds']})
-
+    return render(request, 'dashboard/index.html', {'feeds': get_channel()})

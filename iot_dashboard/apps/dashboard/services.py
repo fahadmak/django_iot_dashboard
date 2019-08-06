b0951192cfe91f787e5aa46d4dc2bbd70f853d27
fahadmak <fahad.makabugo@andela.com>
2019-08-06 16:54:08 +0300

    feat(data visualization): create graph for data
    - add d3.js in base.html
    - add script to create the graphs in base.html file
    
    [Finishes #167556576]

--- iot_dashboard/apps/dashboard/services.py
+++ iot_dashboard/apps/dashboard/services.py
@@ -9,23 +9,4 @@ def get_channel():
     r = requests.get(url, params=params)
     data = r.json()
     feeds = data['feeds']
-    index = 1
-    feed_data = {}
-    light_feeds = []
-    temperature_feeds = []
-    for feed in feeds:
-        light_feed = {
-            'home': index,
-            'value': round(float(feed['field1']), 2)
-        }
-        light_feeds.append(light_feed)
-        temperature_feed = {
-            'home': index,
-            'value': round(float(feed['field2']), 2)
-        }
-        temperature_feeds.append(temperature_feed)
-        index += 1
-    feed_data['light'] = light_feeds
-    feed_data['temperature'] = temperature_feeds
-    feed_data['results'] = params['results']
-    return feed_data
+    return feeds
--- iot_dashboard/apps/dashboard/views.py
+++ iot_dashboard/apps/dashboard/views.py
@@ -12,8 +12,5 @@ CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 @login_required
 # @cache_page(CACHE_TTL)
 def dashboard(request):
-    feed_data = get_channel()
-    light = feed_data['light']
-    temperature = feed_data['temperature']
-    results = feed_data['results']
-    return render(request, 'dashboard/index.html', {'light': light, 'temperature': temperature, 'results': results})
+    feeds = get_channel()
+    return render(request, 'dashboard/index.html', {'feeds': feeds})

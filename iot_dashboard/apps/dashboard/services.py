9e65b9253023997258fbf86c4f3a4c8bf372c236
fahadmak <fahad.makabugo@andela.com>
2019-08-12 16:10:34 +0300

    Revert "#167556576 create graph for data from thingspeak api"

--- iot_dashboard/apps/dashboard/services.py
+++ iot_dashboard/apps/dashboard/services.py
@@ -4,28 +4,9 @@ import requests
 def get_channel():
     url = 'https://api.thingspeak.com/channels/9/feeds.json'
     params = {
-        'results': 20
+        'timescale': 5
     }
     r = requests.get(url, params=params)
     data = r.json()
-    feeds = data['feeds']
-    index = 1
-    feed_data = {}
-    light_feeds = []
-    temperature_feeds = []
-    for feed in feeds:
-        light_feed = {
-            'home': index,
-            'value': round(float(feed['field1']), 2)
-        }
-        light_feeds.append(light_feed)
-        temperature_feed = {
-            'home': index,
-            'value': round(float(feed['field2']), 2)
-        }
-        temperature_feeds.append(temperature_feed)
-        index += 1
-    feed_data['light'] = light_feeds
-    feed_data['temperature'] = temperature_feeds
-    feed_data['results'] = params['results']
-    return feed_data
+    channel = data['feeds']
+    return channel
